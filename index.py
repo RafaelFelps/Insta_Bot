c9f3d5d8275ee4bfbcc04946ea019bcf8749fd65
Rafael Silva <rafaelfelps62@gmail.com>
2020-10-05 16:50:05 -0300

    v.2

--- index.py
+++ index.py
@@ -10,93 +10,126 @@ keyboard = Controller()
 chromedriver_path = 'chromedriver.exe' # Change this to your own chromedriver path!
 webdriver = webdriver.Chrome(executable_path=chromedriver_path)
 sleep(2)
-webdriver.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
+webdriver.get('https://www.instagram.com/accounts/login/')
 sleep(3)
 
+def session_login():
+        #  Faz o Login
+        username = webdriver.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input')
+        username.send_keys('rafaelfelps62')
+        password = webdriver.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input')
+        password.send_keys('R@sfaeu123')
 
-#  Faz o Login
-username = webdriver.find_element_by_name('username')
-username.send_keys('rafaelfelps62')
-password = webdriver.find_element_by_name('password')
-password.send_keys('R@sfaeu123')
-
-# Clica no botão de fazer login
-button_login = webdriver.find_element_by_css_selector('#react-root > section > main > div > article > div > div:nth-child(1) > div > form > div:nth-child(4) > button') 
-button_login.click()
-sleep(3)
-
-
+        # Clica no botão de fazer login
+        button_login = webdriver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button')
+        button_login.click()
+        sleep(3)
 
 
-#  Faz a busca na barra de pesquisa
-search_bar = webdriver.find_element_by_css_selector('#react-root > section > nav > div:nth-child(2) > div > div > div:nth-child(2) > input')
-search_bar.send_keys('popugramsg')
-sleep(2)
-keyboard.press(Key.enter)
-sleep(1)
-keyboard.press(Key.enter)
-sleep(3)
 
-# Clica nos seguidores da página
-following = webdriver.find_element_by_css_selector("#react-root > section > main > div > header > section > ul > li:nth-child(3) > a[href*='/popugramsg/following/']")
-following.click()
-sleep(5)
+def search_page():
+        #  Faz a busca na barra de pesquisa
+        search_bar = webdriver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input')
+        search_bar.send_keys('contabilizei')
+        sleep(2)
+        keyboard.press(Key.enter)
+        sleep(1)
+        keyboard.press(Key.enter)
+        sleep(3)
 
 
-#scroll down modal de seguidores
-l = 0
-while (l < 4):
-        keyboard.press(Key.tab)
-        keyboard.release(Key.tab)
-        l = l + 1
-
-sleep(2) 
-
-n = 0
-while(n <= 5):
-        keyboard.press(Key.down) 
-        keyboard.release(Key.down) 
-        sleep(0.2)
-        n = n+1
-sleep(1)
-
-l = 0
-while (l < 4):
-        keyboard.press(Key.tab)
-        keyboard.release(Key.tab)
-        l = l + 1
-
-sleep(1)   
-n = 0
-while(n < 400):
-        keyboard.press(Key.down) 
-        keyboard.release(Key.down) 
-        sleep(0.1)
-        n = n+1
-
-while True:
-        # Loop que segue todos os seguidores da página
-        j = 1
-        while (j < 170):
-                follow = webdriver.find_element_by_xpath('/html/body/div[4]/div/div/div[2]/ul/div/li['+ str(j) +']/div/div[2]/button')
-                follow.click()
-                j = j+1
-                sleep(30)
-        print("Fim do primeiro loop")
+        # Clica nos seguidores da página
+        following = webdriver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a')
+        following.click()
         sleep(5)
 
+def scroll_down_followers():
+        #scroll down modal de seguidores
+        l = 0
+        while (l <= 2):
+                keyboard.press(Key.tab)
+                keyboard.release(Key.tab)
+                l = l + 1
+
+        sleep(1) 
+
+        n = 0
+        while(n <= 2):
+                keyboard.press(Key.page_down) 
+                keyboard.release(Key.page_down) 
+                sleep(0.2)
+                n = n+1
+        sleep(1)
+
+        l = 0
+        while (l <= 2):
+                keyboard.press(Key.tab)
+                keyboard.release(Key.tab)
+                l = l + 1
+
+        sleep(1)   
+        n = 0
+        while(n < 400):
+                keyboard.press(Key.page_down) 
+                keyboard.release(Key.page_down) 
+                sleep(0.1)
+                n = n+1
+
+
+def follow():
+        x = 1
+        
+        while(x < 150):
+                try:
+                        follow = webdriver.find_element_by_xpath('/html/body/div[4]/div/div/div[2]/ul/div/li['+ str(x) +']/div/div[3]/button')
+                        follow.click()   
+                        print("Seguidor: " + str(x))
+                        timer = randint(1,5)
+                        sleep(timer)
+                        
+                except:
+                        cancel = webdriver.find_element_by_xpath('/html/body/div[5]/div/div/div/div[3]/button[2]')
+                        cancel.click()
+                        print("Seguir o proximo")
+                        x = x-1
+
+                timer = randint(10,70)
+                j = 0
+                sleep(timer)
+
+        x = x+1 
+                        
+
+
+
+def unfollow():
         # Deixa de seguir todo mundo
         k = 1
-        while (k < 170):
-                follow = webdriver.find_element_by_xpath('/html/body/div[4]/div/div/div[2]/ul/div/li['+ str(k) +']/div/div[2]/button')
+        while (k < 150):
+                follow = webdriver.find_element_by_xpath('/html/body/div[4]/div/div/div[2]/ul/div/li['+ str(k) +']/div/div[3]/button')
                 follow.click()
                 k = k+1
-                sleep(30)
+                timer = randint(1,50)
+                keyboard.press(Key.page_down) 
+                keyboard.release(Key.page_down) 
+
+                sleep(timer)
                 unfollow = webdriver.find_element_by_xpath('/html/body/div[5]/div/div/div/div[3]/button[1]')
                 unfollow.click()
                 sleep(0.3)
         print("Fim do segundo loop") 
 
+#Faz o login
+session_login()
 
+#Busca a pagina
+search_page()
 
+#Percorre o modal de seguidores
+scroll_down_followers()
 
+# Segue X pessoas
+follow()
+
+#Deixa de seguir X pessoas
+unfollow()
