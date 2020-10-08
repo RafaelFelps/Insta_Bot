8d14e6be45e2c078ed8940b816c38b43f2da7af1
Rafael Felipe da Silva <48489919+RafaelFelps@users.noreply.github.com>
2020-10-08 11:17:32 -0300

    Finalização do index.py

--- index.py
+++ index.py
@@ -15,12 +15,12 @@ sleep(3)
 
 def session_login():
         #  Faz o Login
-        print("===============")
-        print("=====LOGIN=====")
-        print("===============")
+        print("=============")
+        print("=== LOGIN ===")
+        print("=============")
 
         username = webdriver.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input')
-        username.send_keys('your_login')
+        username.send_keys('your_username')
         password = webdriver.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input')
         password.send_keys('your_password')
 
@@ -32,31 +32,32 @@ def session_login():
 
 
 def search_page():
-        print("================")
-        print("=====SEARCH=====")
-        print("================")
         #  Faz a busca na barra de pesquisa
+        print("=============")
+        print("=== BUSCA ===")
+        print("=============")
         search_bar = webdriver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input')
-        search_bar.send_keys('contabilizei')
+        search_bar.send_keys('portalcontabeis')
+        sleep(3)
+        keyboard.press(Key.enter)
         sleep(2)
         keyboard.press(Key.enter)
-        sleep(1)
-        keyboard.press(Key.enter)
-        sleep(3)
+        sleep(10)
 
 
         # Clica nos seguidores da página
-        following = webdriver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a')
+        # following = webdriver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a')
+        following = webdriver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/header/section/ul/li[3]/a')
         following.click()
         sleep(5)
 
 def scroll_down_followers():
-        print("================")
-        print("=====SCROLL=====")
-        print("================")
+        print("==============")
+        print("=== SCROLL ===")
+        print("==============")
         #scroll down modal de seguidores
         l = 0
-        while (l <= 2):
+        while (l <= 10):
                 keyboard.press(Key.tab)
                 keyboard.release(Key.tab)
                 l = l + 1
@@ -64,7 +65,7 @@ def scroll_down_followers():
         sleep(1) 
 
         n = 0
-        while(n <= 2):
+        while(n <= 10):
                 keyboard.press(Key.page_down) 
                 keyboard.release(Key.page_down) 
                 sleep(0.2)
@@ -72,7 +73,7 @@ def scroll_down_followers():
         sleep(1)
 
         l = 0
-        while (l <= 2):
+        while (l <=10):
                 keyboard.press(Key.tab)
                 keyboard.release(Key.tab)
                 l = l + 1
@@ -86,12 +87,13 @@ def scroll_down_followers():
                 n = n+1
 
 
-def follow():
-        print("================")
-        print("=====FOLLOW=====")
-        print("================")
+def follow(cont=20):
+        print("==============")
+        print("=== FOLLOW ===")
+        print("==============")
+        print("Seguir: " + str(cont))
         x = 1
-        while (x <= 200):
+        while (x <= cont):
                 try:
                         timer = randint(10, 80)
                         sleep(timer)
@@ -103,7 +105,8 @@ def follow():
                         cancel = webdriver.find_element_by_xpath('/html/body/div[5]/div/div/div/div[3]/button[2]')
                         cancel.click()
                         x = x - 1
-                print(str(x))
+            
+                print(x)
                 x = x + 1
 
 
@@ -111,23 +114,38 @@ def follow():
 
 
 
-def unfollow():
-        print("==================")
-        print("=====UNFOLLOW=====")
-        print("==================")
+def unfollow(cont=20): 
+        print("================")
+        print("=== UNFOLLOW ===")
+        print("================")
+        print("Deixar de seguir: " + str(cont))
         # Deixa de seguir todo mundo
         k = 1
-        while (k <= 200):
-                follow = webdriver.find_element_by_xpath('/html/body/div[4]/div/div/div[2]/ul/div/li['+ str(k) +']/div/div[2]/button')
-                follow.click()
-                k = k+1
-                timer = randint(1,50)
 
-                sleep(timer)
-                unfollow = webdriver.find_element_by_xpath('/html/body/div[5]/div/div/div/div[3]/button[1]')
-                unfollow.click()
-                sleep(0.3)
-                print(str(k))
+        while (k < cont):
+                try:
+                        follow = webdriver.find_element_by_xpath('/html/body/div[4]/div/div/div[2]/ul/div/li['+ str(k) +']/div/div[2]/button')
+                        follow.click()
+                        k = k+1
+                        timer = randint(1,50)
+
+                        sleep(timer)
+                        unfollow = webdriver.find_element_by_xpath('/html/body/div[5]/div/div/div/div[3]/button[1]')
+                        unfollow.click()
+                        sleep(3)
+                        print(k)
+                except:
+                        follow = webdriver.find_element_by_xpath('/html/body/div[4]/div/div/div[2]/ul/div/li['+ str(k) +']/div/div[3]/button')
+                        follow.click()
+                        k = k+1
+                        timer = randint(1,50)
+
+                        sleep(timer)
+                        unfollow = webdriver.find_element_by_xpath('/html/body/div[5]/div/div/div/div[3]/button[1]')
+                        unfollow.click()
+                        sleep(3)
+                        print(k)
+        print("Fim do segundo loop") 
 
 
 #Faz o login
@@ -138,10 +156,14 @@ search_page()
 
 #Percorre o modal de seguidores
 scroll_down_followers()
-
+#
 while True:
         # Segue X pessoas
         follow()
 
         #Deixa de seguir X pessoas
         unfollow()
+
+# unfollow()
+
+
